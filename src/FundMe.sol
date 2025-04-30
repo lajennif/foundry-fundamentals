5ced8b9efc12b042b790382b8ce133f8ba6dfae1
lajennif <lajennif@gmail.com>
2025-04-30 12:35:26 -0400

    Initial commit

---
+++ src/FundMe.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Note: The AggregatorV3Interface might be at a different location than what was in the video!
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {console} from "forge-std/Test.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) public s_addressToAmountFunded;
+    address[] public s_funders;
+
+    /** Getter Functions */
+
+    function getAddressToAmountFunded(
+        address fundingAddress
+    ) public view returns (uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return s_funders[index];
+    }
+
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public immutable i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    AggregatorV3Interface private s_priceFeed;
+
+    constructor(address priceFeed) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeed);
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        // AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // return priceFeed.version();
+        return s_priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+
+    // function cheaperWithdraw() public onlyOwner {
+    //     uint256 fundersLength = s_funders.length;
+    //     for (
+    //         uint256 funderIndex = 0;
+    //         funderIndex < fundersLength;
+    //         funderIndex++
+    //     ) {
+    //         address funder = s_funders[funderIndex];
+    //         s_addressToAmountFunded[funder] = 0;
+    //     }
+    //     s_funders = new address[](0);
+
+    //     (bool callSuccess, ) = payable(msg.sender).call{
+    //         value: address(this).balance
+    //     }("");
+    //     require(callSuccess, "Call failed");
+    // }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+    function cheaperWithdraw() public onlyOwner {
+        uint256 fundersLength = s_funders.length;
+
+        console.log("Funders Length: ", fundersLength);
+
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < fundersLength;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            console.log("Resetting funder: ", funder);
+            s_addressToAmountFunded[funder] = 0;
+        }
+
+        // Reset funders array
+        s_funders = new address[](0);
+        console.log("Funders array cleared");
+
+        // Check contract balance before withdrawal
+        uint256 contractBalanceBefore = address(this).balance;
+        console.log(
+            "Contract balance before withdraw: ",
+            contractBalanceBefore
+        );
+
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: contractBalanceBefore
+        }("");
+
+        require(callSuccess, "Call failed");
+        console.log("Funds successfully transferred to owner");
+
+        // Check contract balance after withdrawal
+        uint256 contractBalanceAfter = address(this).balance;
+        console.log("Contract balance after withdraw: ", contractBalanceAfter);
+    }
+
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
+}
+
+// Concepts we didn't cover yet (will cover in later sections)
+// 1. Enum
+// 2. Events
+// 3. Try / Catch
+// 4. Function Selector
+// 5. abi.encode / decode
+// 6. Hash with keccak256
+// 7. Yul / Assembly
---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+    address alice = makeAddr("alice");
+    uint256 constant SEND_VALUE = 1e18; // 1 ETH
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    modifier funded() {
+        vm.deal(alice, STARTING_BALANCE);
+        vm.prank(alice);
+        fundMe.fund{value: SEND_VALUE}();
+        assert(address(fundMe).balance > 0);
+        _;
+    }
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        if (block.chainid == 11155111) {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 4);
+        }
+        if (block.chainid == 1) {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 6);
+        } else {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 4);
+        }
+    }
+
+    function testRevertIfInsufficientFunds() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundDataStructure() public funded {
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(alice);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, alice);
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithdrawFromSingleFunder() public funded {
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.i_owner().balance;
+
+        vm.startPrank(fundMe.i_owner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        uint256 endingOwnerBalance = fundMe.i_owner().balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            endingOwnerBalance,
+            startingOwnerBalance + startingFundMeBalance
+        );
+    }
+
+    // function testWithdrawFromMultipleFunders() public funded {
+    //     uint160 numberOfFunders = 10;
+    //     uint160 startingFunderIndex = 1;
+    //     for (
+    //         uint160 i = startingFunderIndex;
+    //         i < numberOfFunders + startingFunderIndex;
+    //         i++
+    //     ) {
+    //         hoax(address(i), SEND_VALUE);
+    //         fundMe.fund{value: SEND_VALUE}();
+    //     }
+
+    //     uint256 startingFundMeBalance = address(fundMe).balance;
+    //     uint256 startingOwnerBalance = fundMe.i_owner().balance;
+
+    //     vm.startPrank(fundMe.i_owner());
+    //     fundMe.withdraw();
+    //     vm.stopPrank();
+
+    //     console.log("FundMe balance after: %s", address(fundMe).balance);
+    //     console.log("Owner balance after: %s", fundMe.i_owner().balance);
+    //     console.log(
+    //         "Expected: %s",
+    //         startingFundMeBalance + startingOwnerBalance
+    //     );
+
+    //     assert(address(fundMe).balance == 0);
+    //     assert(
+    //         startingFundMeBalance + startingOwnerBalance ==
+    //             fundMe.i_owner().balance
+    //     );
+    //     assert(
+    //         (numberOfFunders + 1) * SEND_VALUE ==
+    //             fundMe.i_owner().balance - startingOwnerBalance
+    //     );
+    // }
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for (
+            uint160 i = startingFunderIndex;
+            i < numberOfFunders + startingFunderIndex;
+            i++
+        ) {
+            // we get hoax from stdcheats
+            // prank + deal
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.i_owner().balance;
+
+        vm.startPrank(fundMe.i_owner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.i_owner().balance
+        );
+        // assert(
+        //     (numberOfFunders + 1) * SEND_VALUE ==
+        //         fundMe.i_owner().balance - startingOwnerBalance
+        // );
+        assertApproxEqAbs(
+            fundMe.i_owner().balance - startingOwnerBalance,
+            (numberOfFunders + 1) * SEND_VALUE,
+            2e17 // 0.001 ETH wiggle room
+        );
+    }
+}
