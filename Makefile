fc5ea8a2660f703dc3fa31a86a9cde05bf3d794a
Jennifer La <jennifer@MacBookPro.attlocal.net>
2025-05-15 17:07:24 -0400

    including anvil deploy

--- Makefile
+++ Makefile
@@ -31,3 +31,6 @@ deploy:
 
 deploy-sepolia:
 	forge script script/DeployFundMe.s.sol:DeployFundMe --rpc-url $(SEPOLIA_RPC_URL) --private-key $(SEPOLIA_PRIVATE_KEY) --broadcast --verify --etherscan-api-key $(ETHERSCAN_API_KEY) -vvvv
+
+deploy-anvil:
+	forge script script/DeployFundMe.s.sol:DeployFundMe --rpc-url $(ANVIL_RPC_URL) --private-key $(ANVIL_PRIVATE_KEY) --broadcast -vvvv
--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol"; // ✅ Import console
 import {FundMe} from "../src/FundMe.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
 
@@ -10,9 +10,15 @@ contract DeployFundMe is Script {
     function run() external returns (FundMe) {
         HelperConfig helperConfig = new HelperConfig();
         address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
-        vm.startBroadcast();
+        address sender = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+        vm.startBroadcast(sender);
         FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
+
+        // ✅ Log the i_owner after deployment
+        console.log("Contract deployed at:", address(fundMe));
+        console.log("Owner (i_owner) is:", fundMe.i_owner()); // i_owner must be public
+
         return fundMe;
     }
 }
